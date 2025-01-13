3d9a18cb31f82676688281f39c67b7a9ef4d9f8c
RitchiDV <hakdigital98@gmail.com>
2025-01-13 11:33:41 -0600

    proyecto de autenticasion y encriptacion

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const expressJwt = require("express-jwt");
+const app = express();
+
+app.use(express.json());
+
 mongoose.connect(
   "mongodb+srv://Hakdigital98:Adivinala123@ricardi.j4xrm.mongodb.net/auth?retryWrites=true&w=majority&appName=Ricardi"
 );
